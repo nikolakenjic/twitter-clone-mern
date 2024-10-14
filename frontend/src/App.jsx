afc658e2b783eb6cbe83e7f6f1406a6b2b58c987
Nikola Kenjic <nikola.nico89@gmail.com>
2024-10-14 19:34:48 +0200

    signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/signup', element: <SignUpPage /> },
+  { path: '/login', element: <LoginPage /> },
+]);
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-xl bg-blue-600 text-gray-100">Text</h1>
+    <div className="flex max-w-6xl mx-auto">
+      <RouterProvider router={router} />
     </div>
   );
 };
