40fe22e84ea29421105e5487447328431fa614e0
Nikola Kenjic <nikola.nico89@gmail.com>
2024-10-16 20:10:46 +0200

    get me

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,23 +7,54 @@ import LoginPage from './pages/LoginPage';
 import ErrorPage from './pages/ErrorPage';
 import NotificationPage from './pages/NotificationPage';
 import ProfilePage from './pages/ProfilePage';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: '/', element: <HomePage /> },
-      { path: '/signup', element: <SignUpPage /> },
-      { path: '/login', element: <LoginPage /> },
-      { path: '/notifications', element: <NotificationPage /> },
-      { path: '/profile/:username', element: <ProfilePage /> },
-    ],
-  },
-]);
+import { useQuery } from '@tanstack/react-query';
+import fetchUrl from './utils/axios';
+import LoadingSpinner from './components/common/LoadingSpinner';
 
 const App = () => {
+  const { data: authUser, isLoading } = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async () => {
+      try {
+        const { data, status } = await fetchUrl.get('/auth/me');
+
+        if (status !== 200) {
+          throw new Error(
+            data.error?.response?.data?.message || 'Failed to logout'
+          );
+        }
+
+        console.log('auth user is here', data);
+        return data;
+      } catch (error) {
+        throw err;
+      }
+    },
+  });
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        { path: '/', element: <HomePage /> },
+        { path: '/signup', element: <SignUpPage /> },
+        { path: '/login', element: <LoginPage /> },
+        { path: '/notifications', element: <NotificationPage /> },
+        { path: '/profile/:username', element: <ProfilePage /> },
+      ],
+    },
+  ]);
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg1" />
+      </div>
+    );
+  }
+
   return <RouterProvider router={router} />;
 };
 
