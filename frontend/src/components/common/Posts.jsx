bb8774bb5d048c341580bfaf664588a4ed4bc01a
Nikola Kenjic <nikola.nico89@gmail.com>
2024-10-16 23:23:05 +0200

    posts fetch

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,25 +1,70 @@
 import Post from './Post';
 import PostSkeleton from '../skeletons/PostSkeleton';
 import { POSTS } from '../../utils/db/dummyData';
+import { useQuery } from '@tanstack/react-query';
+import fetchUrl from '../../utils/axios';
+import { useEffect } from 'react';
 
-const Posts = () => {
-  const isLoading = false;
+const Posts = ({ feedType, username, userId }) => {
+  const getEndPoint = () => {
+    switch (feedType) {
+      case 'forYour':
+        return '/posts/all';
+      case 'following':
+        return '/posts/following';
+      case 'posts':
+        return `/posts/user/${username}`;
+      case 'likes':
+        return `/posts/likes/${userId}`;
+      default:
+        return '/posts/all';
+    }
+  };
+
+  const POST_ENDPOINT = getEndPoint();
+
+  const { data, isLoading, refetch, isRefetching } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      try {
+        const { data, status } = await fetchUrl.get(POST_ENDPOINT);
+
+        if (status !== 200) {
+          throw new Error(
+            data.error?.response?.data?.message || 'Failed to create account'
+          );
+        }
+
+        return data;
+      } catch (err) {
+        console.log('Error 💥', err.response?.data?.message || err.message);
+        throw err;
+      }
+    },
+  });
+
+  // Refetch data when state changes
+  useEffect(() => {
+    refetch();
+  }, [feedType, refetch, username]);
+
+  const { status, posts } = data || {};
 
   return (
     <>
-      {isLoading && (
+      {(isLoading || isRefetching) && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
+      {!isLoading && !isRefetching && posts?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {!isLoading && !isRefetching && posts && (
         <div>
-          {POSTS.map((post) => (
+          {posts.map((post) => (
             <Post key={post._id} post={post} />
           ))}
         </div>
--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,16 +5,11 @@ import { IoNotifications } from 'react-icons/io5';
 import { FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { BiLogOut } from 'react-icons/bi';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import fetchUrl from '../../utils/axios';
 
 const Sidebar = () => {
-  const data = {
-    fullName: 'John Doe',
-    username: 'johndoe',
-    profileImg: '/avatars/boy1.png',
-  };
   const queryClient = useQueryClient();
 
   const {
@@ -48,6 +43,12 @@ const Sidebar = () => {
     },
   });
 
+  const {
+    data: { status, data: autUser },
+  } = useQuery({
+    queryKey: ['authUser'],
+  });
+
   return (
     <div className="md:flex-[2_2_0] w-18 max-w-52">
       <div className="sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full">
@@ -76,7 +77,7 @@ const Sidebar = () => {
 
           <li className="flex justify-center md:justify-start">
             <Link
-              to={`/profile/${data?.username}`}
+              to={`/profile/${autUser?.username}`}
               className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer"
             >
               <FaUser className="w-6 h-6" />
@@ -84,22 +85,22 @@ const Sidebar = () => {
             </Link>
           </li>
         </ul>
-        {data && (
+        {autUser && (
           <Link
-            to={`/profile/${data.username}`}
+            to={`/profile/${autUser.username}`}
             className="mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full"
           >
             <div className="avatar hidden md:inline-flex">
               <div className="w-8 rounded-full">
-                <img src={data?.profileImg || '/avatar-placeholder.png'} />
+                <img src={autUser?.profileImg || '/avatar-placeholder.png'} />
               </div>
             </div>
             <div className="flex justify-between flex-1">
               <div className="hidden md:block">
                 <p className="text-white font-bold text-sm w-20 truncate">
-                  {data?.fullName}
+                  {autUser?.fullName}
                 </p>
-                <p className="text-slate-500 text-sm">@{data?.username}</p>
+                <p className="text-slate-500 text-sm">@{autUser?.username}</p>
               </div>
               <BiLogOut
                 className="w-5 h-5 cursor-pointer"
