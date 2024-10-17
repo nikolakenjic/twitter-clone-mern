2bcaa6b4b5543cbd63b463f7bae8ac4b98824d15
Nikola Kenjic <nikola.nico89@gmail.com>
2024-10-17 20:38:09 +0200

    create post

--- frontend/src/components/home/CreatePost.jsx
+++ frontend/src/components/home/CreatePost.jsx
@@ -2,7 +2,7 @@ import { CiImageOn } from 'react-icons/ci';
 import { BsEmojiSmileFill } from 'react-icons/bs';
 import { useRef, useState } from 'react';
 import { IoCloseSharp } from 'react-icons/io5';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import fetchUrl from '../../utils/axios';
 import toast from 'react-hot-toast';
 
@@ -11,6 +11,9 @@ const CreatePost = () => {
   const [img, setImg] = useState(null);
 
   const imgRef = useRef(null);
+
+  const { data } = useQuery({ queryKey: ['authUser'] });
+  const authUser = data.data;
 
   const queryCLient = useQueryClient();
 
@@ -26,7 +29,6 @@ const CreatePost = () => {
           text,
           img,
         });
-        console.log(status);
 
         if (status !== 201) {
           throw new Error(
@@ -48,10 +50,6 @@ const CreatePost = () => {
     },
   });
 
-  const data = {
-    profileImg: '/avatars/boy1.png',
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     createPost({ text, img });
@@ -72,7 +70,7 @@ const CreatePost = () => {
     <div className="flex p-4 items-start gap-4 border-b border-gray-700">
       <div className="avatar">
         <div className="w-8 rounded-full">
-          <img src={data.profileImg || '/avatar-placeholder.png'} />
+          <img src={authUser.profileImg || '/avatar-placeholder.png'} />
         </div>
       </div>
       <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
@@ -117,7 +115,13 @@ const CreatePost = () => {
             {isPending ? 'Posting...' : 'Post'}
           </button>
         </div>
-        {isError && <div className="text-red-500">Something went wrong</div>}
+        {isError && (
+          <div className="text-red-500">
+            {error?.response?.data?.message ||
+              error?.message ||
+              'An unexpected error occurred'}
+          </div>
+        )}
       </form>
     </div>
   );
